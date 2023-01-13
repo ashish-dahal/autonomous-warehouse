951b1562e5beaa439fb9363d3ec2f676d2bb9378
Daria Butyrskaya <90958123+dariabutyrskaya@users.noreply.github.com>
2023-01-13 16:48:06 +0100

    Add files via upload

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,16 +1,34 @@
-def has_obstacle_in_path(grid, path):
-    # get the dimensions of the grid
-    rows, cols = len(grid), len(grid[0])
+from typing import List, Tuple
 
-    # iterate through the path and check if any point in the path lies on an obstacle
-    for x, y in path:
-        # check if the coordinates are valid (i.e., within the bounds of the grid)
-        if x < 0 or x >= rows or y < 0 or y >= cols:
-            return True  # invalid coordinates, return True
 
-        # check if there is an obstacle at the current point in the path
-        if grid[x][y] == 1:
-            return True  # obstacle found, return True
+class Analyzer:
+    def __init__(self, map: List[List[int]], knowledge):
+        self.map = map
+        self.knowledge = knowledge
 
-    # no obstacle found in the path
-    return False
+    def detect_obstacles(self):
+        """ Detects new obstacles in the map. """
+        # Iterate over the map and check for obstacles
+        for i in range(len(self.map)):
+            for j in range(len(self.map[0])):
+                if self.map[i][j] == 1:
+                    # Check if this obstacle was already known
+                    if (i, j) not in self.knowledge.obstacles:
+                        # If not, add it to the list of known obstacles
+                        self.knowledge.obstacles.append((i, j))
+                        # Update the knowledge database with the new obstacle
+                        self.knowledge.update_database((i, j), "obstacle")
+
+    def assign_package(self, package: Tuple[Tuple[int, int], Tuple[int, int], int]):
+        """ Assigns a package to a robot based on its priority. """
+        # Sort the list of available packages by priority
+        sorted_packages = sorted(self.knowledge.packages, key=lambda x: x[2], reverse=True)
+        # Assign the package with the highest priority to a robot
+        self.knowledge.assign_package(sorted_packages[0][0])
+
+    def get_highest_priority_package(self):
+        """ Returns the ID of the package with the highest priority. """
+        # Sort the list of available packages by priority
+        sorted_packages = sorted(self.knowledge.packages, key=lambda x: x[2], reverse=True)
+        # Return the ID of the package with the highest priority
+        return sorted_packages[0][0]
