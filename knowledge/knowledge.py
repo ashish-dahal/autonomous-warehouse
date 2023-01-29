418137d286754a09bcd9830e86dbbfcadb151c93
ashish-dahal <ashishdahal1997@gmail.com>
2023-01-29 17:32:43 +0100

    Update monitor

--- knowledge/knowledge.py
+++ knowledge/knowledge.py
@@ -4,49 +4,61 @@ import json
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://admin:admin@cluster0.nwcrbvw.mongodb.net/?retryWrites=true&w=majority") #connecting to MongoDB
+# connecting to MongoDB
+client = MongoClient(
+    "mongodb+srv://admin:admin@cluster0.nwcrbvw.mongodb.net/?retryWrites=true&w=majority")
 
 db = client["autodb"]
-#4 collections: map_info, package_info, robot_info, planned_path
+# 4 collections: map_info, package_info, robot_info, planned_path
 collection_map = db["map_info"]
 collection_package = db["package_info"]
 collection_robot = db["robot_info"]
 collection_planned_path = db["planned_path"]
 
+
 @app.route('/map_info', methods=['GET', 'POST'])
 def map_info():
-    if request.method == 'POST': #insert map_info document
+    if request.method == 'POST':  # insert map_info document
         map_info = request.get_json()
-        collection_map.insert_one({"map" : map_info['map']}).inserted_id
-    else: #fetch last map_info document
-        last_map_document = json.loads(collection_map.find().sort({_id:-1}).limit(1))
+        collection_map.insert_one({"map": map_info['map']}).inserted_id
+    else:  # fetch last map_info document
+        last_map_document = json.loads(
+            collection_map.find().sort({_id: -1}).limit(1))
         return json.dumps(last_map_document)
 
 
 @app.route('/package_info', methods=['GET', 'POST'])
 def package_info():
-    if request.method == 'POST': #insert package_info document
+    if request.method == 'POST':  # insert package_info document
         package_info = request.get_json()
-        collection_package.insert_one({"id_package" : package_info['id_package'], "source":  package_info['source'], "destination": package_info['destination'], "priority": package_info['priority']}).inserted_id
-    else: #fetch first package_info document (FIFO)
-        first_package_document = json.loads(collection_package.find().sort({_id:1}).limit(1))
-        collection_package.remove(first_package_element)
-        return json.dumps(last_package_document)
+        collection_package.insert_one({"id_package": package_info['id_package'], "source":  package_info['source'],
+                                      "destination": package_info['destination'], "priority": package_info['priority']}).inserted_id
+    else:  # fetch first package_info document (FIFO)
+        first_package_document = json.loads(
+            collection_package.find().sort({_id: 1}).limit(1))
+        collection_package.remove(first_package_document)
+        return json.dumps(first_package_document)
+
 
 @app.route('/robot_info', methods=['GET', 'POST'])
 def robot_info():
-    if request.method == 'POST': #insert robot_info document
+    if request.method == 'POST':  # insert robot_info document
         robot_info = request.get_json()
-        collection_robot.insert_one({"state" : robot_info['state'], "position" :  robot_info['position']})
-    else: #fetch last robot_info document
-        last_robot_document = json.loads(collection_robot.find().sort({_id:-1}).limit(1))
+        collection_robot.insert_one(
+            {"state": robot_info['state'], "position":  robot_info['position']})
+    else:  # fetch last robot_info document
+        last_robot_document = json.loads(
+            collection_robot.find().sort({_id: -1}).limit(1))
         return json.dumps(last_robot_document)
+
 
 @app.route('/planned_path', methods=['GET', 'POST'])
 def planned_path():
-    if request.method == 'POST': #insert planned_path document
+    if request.method == 'POST':  # insert planned_path document
         planned_path = request.get_json()
-        collection_planned_path.insert_one({"package_id" : planned_path['package_id'], "planned_path" : planned_path['planned_path']})
-    else: #fetch last planned_path document
-        last_path_document = json.loads(collection_planned_path.find().sort({_id:-1}).limit(1))
+        collection_planned_path.insert_one(
+            {"package_id": planned_path['package_id'], "planned_path": planned_path['planned_path']})
+    else:  # fetch last planned_path document
+        last_path_document = json.loads(
+            collection_planned_path.find().sort({_id: -1}).limit(1))
         return json.dumps(last_path_document)
--- monitor/monitor.py
+++ monitor/monitor.py
@@ -3,24 +3,19 @@ import requests
 import json
 import paho.mqtt.client as mqtt
 
-MQTT_BROKER = "127.0.0.1" 
-MQTT_PORT = 1883 
-MQTT_TOPIC = [("warehouse/map",0),("warehouse/robot/position",0), ("warehouse/package/insert",0)]
+MQTT_BROKER = "127.0.0.1"
+MQTT_PORT = 1883
+MQTT_TOPIC = [("warehouse/map", 0), ("warehouse/robot/position",
+                                     0), ("warehouse/package/insert", 0)]
+
 
 class Monitor:
     def __init__(self, MQTT_BROKER, MQTT_PORT):
         self.client = mqtt.Client()
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-        # Connect to the mqtt broker
-        self.client.connect(MQTT_BROKER, MQTT_PORT)
-        self.client.subscribe(MQTT_TOPIC)
-        
+
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
             print("Connected to broker")
-            global Connected
-            Connected = True
         else:
             print("Connection failed")
 
@@ -29,14 +24,32 @@ class Monitor:
         receive = data.decode("utf-8")
         msg = json.loads(receive)
         if message.topic == "warehouse/map":
-            requests.post(url = "knowledge/map_info", data = msg)
-        elif message.topic == "warehouse/robot/position":
-            requests.post(url = "knowledge/robot_info", data = msg)
+            self.insert_map_info(msg)
+        elif message.topic == "warehouse/robot/status":
+            self.insert_robot_info(msg)
         elif message.topic == "warehouse/package/insert":
-            requests.post(url = "knowledge/package_info", data = msg)
-            
+            self.insert_package_info(msg)
+
+    def insert_map_info(self, map):
+        requests.post(url="knowledge/map_info", data=map)
+
+    def insert_robot_info(self, robot):
+        requests.post(url="knowledge/robot_info", data=robot)
+
+    def insert_package_info(self, package):
+        requests.post(url="knowledge/package_info", data=package)
+
+    def start(self):
+        self.client.connect(MQTT_BROKER, MQTT_PORT)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        # Connect to the mqtt broker
+        self.client.subscribe(MQTT_TOPIC)
+        monitor.client.loop_start()
+
+
 if __name__ == "__main__":
     monitor = Monitor(MQTT_BROKER, MQTT_PORT)
-    monitor.client.loop_start()
+    monitor.start()
     while True:
         time.sleep(1)
