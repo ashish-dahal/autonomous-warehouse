86883bb2eb1c3099070ddc13c095fbfd3cb5617f
ashish-dahal <ashishdahal1997@gmail.com>
2023-01-29 16:06:12 +0100

    Update analyzer

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,22 +1,11 @@
 import paho.mqtt.client as mqtt
 import requests
 
-<<<<<<< HEAD
-
-class Analyzer:
-    def __init__(self, map, package_info, robot_info):
-        self.map = map
-
-    '''
-    Function that gets data via Rest API from Knowledge component
-    '''
-=======
 
 class Analyzer:
     def __init__(self):
         self.map = None
         self.map_temp = None
->>>>>>> c81d80ac83d325523675a487b90a6c00a4e38511
 
     def get_info(self):
         # saving map to check if it was change (for example, if user added the obstacle to the map)
@@ -24,55 +13,16 @@ class Analyzer:
         # getting the map
         endpoint_map = "/map_info"
         # Send the GET request
-<<<<<<< HEAD
-        self.map = requests.get(endpoint_map)
-
-        '''
-        Function that checks if the map was changed, for example if the user added new obstacle to the map.
-        This code compares the two maps element by element and check if any of the elements are different,
-        if it finds any difference it returns True otherwise False.
-        '''
-
-        def check_map(self):
-            has_changes = any(val1 != val2 for row1, row2 in zip(
-                map_temp, self.map) for val1, val2 in zip(row1, row2))
-            return has_changes
-
-        '''
-        Function that checks if the map was changed, for example if the user added new obstacle to the map
-        '''
-
-        def check_map(self):
-            has_changes = any(val1 != val2 for row1, row2 in zip(
-                map_temp, map) for val1, val2 in zip(row1, row2))
-            return has_changes
-
- def on_connect(client, userdata, flags, rc):
-        print("Connected with result code " + str(rc))
-
-=======
         self.map = requests.get(endpoint_map).json()
->>>>>>> c81d80ac83d325523675a487b90a6c00a4e38511
 
     def check_map(self):
         if self.map_temp is None:
             return False
         # compares the two maps element by element and check if any of the elements are different
-        has_changes = any(val1 != val2 for row1, row2 in zip(self.map_temp, self.map) for val1, val2 in zip(row1, row2))
+        has_changes = any(val1 != val2 for row1, row2 in zip(
+            self.map_temp, self.map) for val1, val2 in zip(row1, row2))
         return has_changes
 
-
-<<<<<<< HEAD
-if changed:
-    client = mqtt.Client()
-    # Connect to the MQTT broker
-    client.connect("MQTT_broker", 1883)
-    # Publish the JSON file to the broker on a specific topic
-    client.publish("warehouse/analyzer/command", "Start")
-    client.disconnect()
-else:
-    None
-=======
 
 if __name__ == "__main__":
 
@@ -89,4 +39,3 @@ if __name__ == "__main__":
         client.disconnect()
     else:
         pass
->>>>>>> c81d80ac83d325523675a487b90a6c00a4e38511
